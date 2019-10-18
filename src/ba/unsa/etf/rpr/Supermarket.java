79ba34fe59312214b6ddf2a551b5857f72b2d644
Aldin Alic <aalic7@etf.unsa.ba>
2019-10-18 19:30:46 +0200

    Prepravke u klasama supermarket i korpa

--- src/ba/unsa/etf/rpr/Supermarket.java
+++ src/ba/unsa/etf/rpr/Supermarket.java
@@ -18,6 +18,9 @@ public class Supermarket {
                 Artikl temp = new Artikl(artikli[i].naziv, artikli[i].cijena, artikli[i].kod);
                 for (int j = i; j < broj_el-1; j++) artikli[j] = artikli[j+1];
                 broj_el = broj_el - 1;
+                artikli[broj_el] = null;    // OVO JE JAKO BITNO, ZBOG RANGOVSKE PETLJE, JER DA NISMO STAVILI NA NULL, ZADNJI OBJEKAT
+                // BI SE SMATRA0 DA PRIPADA NIZU, IAKO SMO SMANJILI BROJ_ELEMENATA NIZA
+                // DA JE ISLO PREKO OBICNE PETLJE, NE BI SE ISPISAO, JER SE ON NALAZI NA POZICIJI artikli[broj_el]
                 return temp;
             }
         }
