619f2df5e23a38aed1dda7e53dffc778e861f597
Aldin Alic <aalic7@etf.unsa.ba>
2019-10-21 14:31:50 +0200

    Testovi za klasu korpa

---
+++ test/ba/unsa/etf/rpr/KorpaTest.java
@@ -0,0 +1,21 @@
+package ba.unsa.etf.rpr;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class KorpaTest {
+
+    @Test
+    void provjeraDodavanjaArtikla() {
+        Supermarket supermarket = new Supermarket();
+        supermarket.dodajArtikl(new Artikl("Ovo je test", 399, "1"));
+        Artikl[] artikli = supermarket.getArtikli();
+
+        assertTrue(artikli[0] != null);
+        for (int i = 1; i < 50; i++) {
+            assertTrue(artikli[i] == null);
+        }// ovaj sad provjerava da li smo dodali prvi element, a s tim da su ostali null
+    }
+
+}
---
+++ test/ba/unsa/etf/rpr/MainTest.java
@@ -0,0 +1,20 @@
+package ba.unsa.etf.rpr;
+
+import org.junit.jupiter.api.Test;
+
+import java.io.ByteArrayOutputStream;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class MainTest {
+
+    @Test
+    void ispisArtikala() {
+
+    }
+
+    @Test
+    void main() {
+
+    }
+}
